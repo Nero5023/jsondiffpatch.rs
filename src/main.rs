3044241e46f14f3f08b8cbda98f9d7dc1b483ca3
Chenhao Zuo <nero5023@gmail.com>
2022-07-17 22:43:13 -0700

    add test for lcs

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,6 @@ fn main() {
             "key0": "name1",
             "c": ["2", "3", "a", "b", "c", "d", "e"]
         }"#;
-    let data = r#"{"c": ["1","a", "c", "e", "f"]}"#;
-    let data1 = r#"{"c": ["2", "3", "a", "b", "c", "d", "e"]}"#;
     let json = read_json_str(data).unwrap();
     let json1 = read_json_str(data1).unwrap();
     let diffs = Vec::new();
@@ -92,7 +90,7 @@ fn diff_json(jval0: &Value, jval1: &Value, mut diffs: Vec<DiffElem>, path: Path)
                     old_val: jval0.clone(),
                     new_val: jval1.clone(),
                 },
-                path: path,
+                path,
             });
             diffs
         }
@@ -370,5 +368,85 @@ mod tests {
         let arr1: Vec<char> = "ace".chars().collect();
         let res = lcs(&arr0, &arr1);
         assert_eq!(res, vec![(0, 0), (2, 1), (4, 2)]);
+    }
+
+    fn str2vec(s: &str) -> Vec<char> {
+        s.chars().collect()
+    }
+
+    #[test]
+    fn test_lcs_empty_and_another() {
+        let lcs1 = lcs(&str2vec(""), &str2vec("abcdef"));
+        assert_eq!(lcs1.len(), 0);
+        let lcs2 = lcs(&str2vec("abcdef"), &str2vec(""));
+        assert_eq!(lcs2.len(), 0);
+    }
+
+    #[test]
+    fn test_lcs_two_same_seq() {
+        let s = "abcde";
+        let lcs_vec = lcs(&str2vec(s), &str2vec(s));
+        assert_eq!(lcs_vec, vec![(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]);
+    }
+
+    #[test]
+    fn test_lcs_prefix_seq() {
+        let s = "abcdef";
+        let prefix = "abc";
+        let lcs1 = lcs(&str2vec(s), &str2vec(prefix));
+        assert_eq!(lcs1, vec![(0, 0), (1, 1), (2, 2)]);
+
+        let lcs2 = lcs(&str2vec(prefix), &str2vec(s));
+        assert_eq!(lcs2, vec![(0, 0), (1, 1), (2, 2)]);
+    }
+
+    #[test]
+    fn test_lcs_no_common_seq() {
+        let lcs_vec = lcs(&str2vec("abcdef"), &str2vec("ghijkl"));
+        assert_eq!(lcs_vec.len(), 0);
+    }
+
+    #[test]
+    fn test_lcs_same_in_mid() {
+        let s1 = "abcdefgh";
+        let s2 = "bdeg";
+        let lcs_vec = lcs(&str2vec(s1), &str2vec(s2));
+        assert_eq!(lcs_vec, vec![(1, 0), (3, 1), (4, 2), (6, 3)]);
+    }
+
+    #[test]
+    fn test_lcs_repeat_elem() {
+        let s1 = "abcbdbebf";
+        let s2 = "bbbb";
+        let lcs_vec = lcs(&str2vec(s1), &str2vec(s2));
+        assert_eq!(lcs_vec, vec![(1, 0), (3, 1), (5, 2), (7, 3)]);
+    }
+
+    #[test]
+    fn test_lcs_non_unique_elem() {
+        let s1 = "abcdabcd";
+        let s2 = "gabhakbf";
+        let lcs_vec = lcs(&str2vec(s1), &str2vec(s2));
+        assert_eq!(lcs_vec, vec![(0, 1), (1, 2), (4, 4), (5, 6)]);
+    }
+
+    #[test]
+    fn test_lcs_common_pre_suf() {
+        let s1 = "abctotodef";
+        let s2 = "abctatatadef";
+        let lcs_vec = lcs(&str2vec(s1), &str2vec(s2));
+        assert_eq!(
+            lcs_vec,
+            vec![
+                (0, 0),
+                (1, 1),
+                (2, 2),
+                (3, 3),
+                (5, 5),
+                (7, 9),
+                (8, 10),
+                (9, 11)
+            ]
+        );
     }
 }
